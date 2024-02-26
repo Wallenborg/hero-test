8ba7e0205bd85ac23edc8eb477341af3798ca5a8
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-02-26 11:08:32 +0100

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,53 @@
-import "./App.css";
+// import "./App.css";
+// import Hero from "./components/Hero/Hero";
+// import Divider from "./components/Divider/Divider";
+// import FeaturedBlogPostList from "./components/FeaturedBlogPost/FeaturedBlogPostList";
+// import Banner from "./components/Banner/Banner";
+// import BlogPostList from "./components/BlogPostList/BlogPostList";
+// import Footer from "./components/Footer/Footer";
+
+// function App() {
+//   return (
+//     <>
+//       <Hero />
+//       <Divider text={"Featured Blog Post"} />
+//       <FeaturedBlogPostList />
+//       <Banner />
+//       <Divider text={"Blog Post"} />
+//       <BlogPostList />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./components/Hero/Hero";
 import Divider from "./components/Divider/Divider";
 import FeaturedBlogPostList from "./components/FeaturedBlogPost/FeaturedBlogPostList";
 import Banner from "./components/Banner/Banner";
 import BlogPostList from "./components/BlogPostList/BlogPostList";
 import Footer from "./components/Footer/Footer";
+import BlogPost from "./components/Blogpost/BlogPost";
 
 function App() {
   return (
-    <>
-      <Hero />
-      <Divider text={"Featured Blog Post"} />
-      <FeaturedBlogPostList />
-      <Banner />
-      <Divider text={"Blog Post"} />
-      <BlogPostList />
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        {/* <> */}
+        {/* <Hero />
+        <Divider text={"Featured Blog Post"} />
+        <FeaturedBlogPostList />
+        <Banner />
+        <Divider text={"Blog Post"} /> */}
+        <Route path="/blog/:postId" element={BlogPost} />
+        <Route exact path="/blog" element={BlogPostList} />
+        {/* <Footer /> */}
+        {/* </> */}
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/FeaturedBlogPost/FeaturedBlogPostList.jsx
+++ src/components/FeaturedBlogPost/FeaturedBlogPostList.jsx
@@ -1,32 +1,72 @@
+// import FeaturedBlogPost from "./FeaturedBlogPost";
+// import "./FeaturedBlogPost.css";
+// import { blogPosts } from "../../data";
+
+// function FeaturedBlogPostList() {
+//   console.log("Imported blogPosts:", blogPosts);
+
+//   // Choose which three blog posts to display by specifying their indexes
+//   const postIndexesToShow = [0, 4, 2];
+
+//   // Filter the blogPosts array based on the selected indexes
+//   const featuredPosts = postIndexesToShow.map((index) => blogPosts[index]);
+
+//   return (
+//     <section>
+//       <nav>
+//         {/* Display the selected featured blog posts */}
+//         <ul className="container-all-blog-post">
+//           {featuredPosts.map((post) => (
+//             <li>
+//               <FeaturedBlogPost
+//                 key={post.id}
+//                 title={post.title}
+//                 sellingSentence={post.sellingSentence}
+//                 imageUrl={post.imageUrl}
+//                 altText={post.altText}
+//                 author={post.author}
+//                 link={post.link}
+//               />
+//             </li>
+//           ))}
+//         </ul>
+//       </nav>
+//     </section>
+//   );
+// }
+
+// export default FeaturedBlogPostList;
+
+import React from "react";
+import { Link } from "react-router-dom";
 import FeaturedBlogPost from "./FeaturedBlogPost";
-import "./FeaturedBlogPost.css";
 import { blogPosts } from "../../data";
+import "./FeaturedBlogPost.css";
 
 function FeaturedBlogPostList() {
-  console.log("Imported blogPosts:", blogPosts);
-
-  // Choose which three blog posts to display by specifying their indexes
   const postIndexesToShow = [0, 4, 2];
-
-  // Filter the blogPosts array based on the selected indexes
   const featuredPosts = postIndexesToShow.map((index) => blogPosts[index]);
 
   return (
     <section>
-      <div className="container-all-blog-post">
-        {/* Display the selected featured blog posts */}
-        {featuredPosts.map((post) => (
-          <FeaturedBlogPost
-            key={post.id}
-            title={post.title}
-            sellingSentence={post.sellingSentence}
-            imageUrl={post.imageUrl}
-            altText={post.altText}
-            author={post.author}
-            link={post.link}
-          />
-        ))}
-      </div>
+      <nav>
+        <ul className="container-all-blog-post">
+          {featuredPosts.map((post) => (
+            <li key={post.id}>
+              <Link to={`/blog/${post.id}`}>
+                <FeaturedBlogPost
+                  title={post.title}
+                  sellingSentence={post.sellingSentence}
+                  imageUrl={post.imageUrl}
+                  altText={post.altText}
+                  author={post.author}
+                  link={post.link}
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </section>
   );
 }
