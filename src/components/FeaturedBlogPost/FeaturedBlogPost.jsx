f070dd268d0acd94b0d175e8626ce850dbfefde0
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-02-26 13:48:28 +0100

    fix link with JS

--- src/components/FeaturedBlogPost/FeaturedBlogPost.jsx
+++ src/components/FeaturedBlogPost/FeaturedBlogPost.jsx
@@ -1,55 +1,20 @@
-// import "./FeaturedBlogPost.css";
-
-// function FeaturedBlogPost({
-//   title,
-//   sellingSentence,
-//   imageUrl,
-//   altText,
-//   author,
-//   link,
-// }) {
-//   return (
-//     <a href={link} aria-label={`Read more about ${title} by ${author}`}>
-//       <div className="container-blog-post">
-//         <div className="container-blog-post-title">
-//           <img src={imageUrl} alt={altText} className="blog-post--icon" />
-//           <h2 className="blog-post--title">{title}</h2>
-//         </div>
-//         <p className="blog-post--text">{sellingSentence}</p>
-//         <p className="blog-post--by">{`By: ${author}`}</p>
-//       </div>
-//     </a>
-//   );
-// }
-
-// export default FeaturedBlogPost;
-
-import React from "react";
-import { Link } from "react-router-dom";
-import "./FeaturedBlogPost.css";
-
 function FeaturedBlogPost({
-  title,
-  sellingSentence,
-  imageUrl,
-  altText,
-  author,
-  link,
+  post: { id, title, sellingSentence, imageUrl, altText, author, link },
+  scrollToPost,
 }) {
+  const handleClick = () => {
+    scrollToPost(id);
+  };
+
   return (
-    <Link
-      to={`/blog/${link}`}
-      aria-label={`Read more about ${title} by ${author}`}
-    >
-      <div className="container-blog-post">
-        <div className="container-blog-post-title">
-          <img src={imageUrl} alt={altText} className="blog-post--icon" />
-          <h2 className="blog-post--title">{title}</h2>
-        </div>
-        <p className="blog-post--text">{sellingSentence}</p>
-        <p className="blog-post--by">{`By: ${author}`}</p>
+    <div className="container-blog-post" onClick={handleClick}>
+      <div className="container-blog-post-title">
+        <img src={imageUrl} alt={altText} className="blog-post--icon" />
+        <h2 className="blog-post--title">{title}</h2>
       </div>
-    </Link>
+      <p className="blog-post--text">{sellingSentence}</p>
+      <p className="blog-post--by">{`By: ${author}`}</p>
+    </div>
   );
 }
 
--- src/components/FeaturedBlogPost/FeaturedBlogPostList.jsx
+++ src/components/FeaturedBlogPost/FeaturedBlogPostList.jsx
@@ -1,51 +1,19 @@
-// import FeaturedBlogPost from "./FeaturedBlogPost";
-// import "./FeaturedBlogPost.css";
-// import { blogPosts } from "../../data";
-
-// function FeaturedBlogPostList() {
-//   console.log("Imported blogPosts:", blogPosts);
-
-//   // Choose which three blog posts to display by specifying their indexes
-//   const postIndexesToShow = [0, 4, 2];
-
-//   // Filter the blogPosts array based on the selected indexes
-//   const featuredPosts = postIndexesToShow.map((index) => blogPosts[index]);
-
-//   return (
-//     <section>
-//       <nav>
-//         {/* Display the selected featured blog posts */}
-//         <ul className="container-all-blog-post">
-//           {featuredPosts.map((post) => (
-//             <li>
-//               <FeaturedBlogPost
-//                 key={post.id}
-//                 title={post.title}
-//                 sellingSentence={post.sellingSentence}
-//                 imageUrl={post.imageUrl}
-//                 altText={post.altText}
-//                 author={post.author}
-//                 link={post.link}
-//               />
-//             </li>
-//           ))}
-//         </ul>
-//       </nav>
-//     </section>
-//   );
-// }
-
-// export default FeaturedBlogPostList;
-
-import React from "react";
-import { Link } from "react-router-dom";
-import FeaturedBlogPost from "./FeaturedBlogPost";
 import { blogPosts } from "../../data";
+import FeaturedBlogPost from "./FeaturedBlogPost";
 import "./FeaturedBlogPost.css";
 
 function FeaturedBlogPostList() {
+  console.log("Imported blogPosts:", blogPosts);
+
   const postIndexesToShow = [0, 4, 2];
   const featuredPosts = postIndexesToShow.map((index) => blogPosts[index]);
+
+  const scrollToPost = (postId) => {
+    const anchorPoint = document.getElementById(postId);
+    if (anchorPoint) {
+      anchorPoint.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
 
   return (
     <section>
@@ -53,16 +21,7 @@ function FeaturedBlogPostList() {
         <ul className="container-all-blog-post">
           {featuredPosts.map((post) => (
             <li key={post.id}>
-              <Link to={`/blog/${post.id}`}>
-                <FeaturedBlogPost
-                  title={post.title}
-                  sellingSentence={post.sellingSentence}
-                  imageUrl={post.imageUrl}
-                  altText={post.altText}
-                  author={post.author}
-                  // link={post.link}
-                />
-              </Link>
+              <FeaturedBlogPost post={post} scrollToPost={scrollToPost} />
             </li>
           ))}
         </ul>
